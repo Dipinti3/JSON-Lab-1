1650b5ce50f58fb1188c297274cc08128c839d5a
Dipinti <dipinti@zipcodes-MBP.fios-router.home>
2021-08-11 16:01:38 -0400

    fixed style.css error

--- js/colors.js
+++ js/colors.js
@@ -4,25 +4,25 @@ var colorsArray = {
             name:"green",
             red:64,
             green:175,
-            blue:63,
+            blue:63
         },
         {
             name:"yellow",
             red:239,
             green:183,
-            blue:62,
+            blue:62
         },
         {
             name:"red",
             red:192,
             green:46,
-            blue:35,
+            blue:35
         },
         {
             name:"blue",
             red:19,
             green:71,
-            blue:144,
+            blue:144
         },
         {
             name="orange",
